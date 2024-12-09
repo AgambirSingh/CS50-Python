b275f90683a46b2882ae8b3b7c2fd547c5b32e8a
Agambir Singh <sinagamb@sheridancollege.ca>
2024-12-09 00:59:10 -0500

    4.2

---
+++ Week4-Libraries/figlet.py
@@ -0,0 +1,17 @@
+import sys
+from pyfiglet import Figlet
+
+# figlet = Figlet()
+# figlet.getFonts()
+# figlet.setFont(font=f)#f is the font’s name as a str:
+# print(figlet.renderText(s))
+
+#Prompts the user for a str of text.
+print("het" , sys.argv[1])
+
+
+#Expects zero or two command-line arguments use if-else
+
+
+
+#Outputs that text in the desired font.
