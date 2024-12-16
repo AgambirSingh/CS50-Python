9bfe747aad3861c8c3179cf66b2705937d73012d
Agambir Singh <sinagamb@sheridancollege.ca>
2024-12-16 03:03:20 -0500

    W4 Sol 4.3 adieu (40min)

--- Week4-Libraries/adieu.py
+++ Week4-Libraries/adieu.py
@@ -1,9 +1,17 @@
-names={}
+import inflect
+
+names=[] #List for user to add names
+
 while True:
+    #get names from user
     try:
-        name=input("Name: ")
+        name = input("Name: ")
         names.append(name)
-        
-    except EOFError:
-        print(name)
+
+    except EOFError: #Press ctrl+D to stop the loop
+        print()
         break
+
+# inflect is module and .engine().join is fucntion used to add (, and) in sentences
+mylist = inflect.engine().join((names))
+print("Adieu, adieu, to " + mylist)
